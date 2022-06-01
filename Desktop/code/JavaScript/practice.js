ba8cf11b9d8a1c7963dfd301d6aef2c04d6f65a2
unknown <es4402@korea.ac.kr>
2022-06-02 00:50:34 +0900

    js_session2

---
+++ Desktop/code/JavaScript/practice.js
@@ -0,0 +1,19 @@
+
+document
+    .querySelectorAll(".list")[0]
+    .addEventListener("click", function (e) {
+        tabBtn(e.target.dataset.id);
+    });
+
+function tabBtn(a) {
+    document.querySelectorAll(".tab-button")[a].addEventListener("click", function () {
+        for( let i =0; i<document.querySelectorAll(".tab-button").length; i++) {
+            document.querySelectorAll(".tab-button")[i].classList.remove("here");
+            document.querySelectorAll(".tab-content")[i].classList.remove("show");
+        }
+
+    document.querySelectorAll(".tab-button")[a].classList.add("here");
+    document.querySelectorAll(".tab-content")[a].classList.add("show");
+    });
+}
+
